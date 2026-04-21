b8dea3588b38f66960e4a1a5fde31fd72b1a3d2d
Tanujay Singh <tanujaysingh063@gmail.com>
2026-04-21 22:49:30 +0530

    updated the bot to use user input urls to search

--- main.py
+++ main.py
@@ -1,47 +1,85 @@
+from flask import Flask, request
 import requests
 from bs4 import BeautifulSoup
-import time
-from datetime import datetime
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-url = "https://www.hmtwatches.in/product_details?id=eyJpdiI6Im5vM0xIYmd2SHZzVGE2ejVwc1FITEE9PSIsInZhbHVlIjoiTlROVTN4UkJFNU5kcUxDZ1VDZXNodz09IiwibWFjIjoiZjFhZDBmMDJiYjFjYWY5ODdiNzhiZWMwMDYxZWU0ZDE5OWVkZDNjZTU3Y2EyZGE2MDczZjIzYzI2ZjQ1ZTYzOSIsInRhZyI6IiJ9"
-
-headers = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120 Safari/537.36"
-}
+app = Flask(__name__)
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
+# CHAT_ID = os.getenv("CHAT_ID")
 
-def send_telegram(msg):
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    data = {
-        "chat_id": CHAT_ID,
-        "text": msg
-    }
-    requests.post(url, data=data)
+@app.route("/")
+def home():
+    return "Bot is running"
 
-while True:
-    current_hour = datetime.now().hour
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.json
+    print("webhook")
+    print(data)
 
-    if 9 <= current_hour < 20:
+    message = data.get("message", {})
+    text = message.get("text") or message.get("caption")
+    chat_id = message.get("chat", {}).get("id")
 
-        response = requests.get(url, headers=headers)
+    if not text:
+        return "ok", 200
+
+    parts = text.strip().split()
+
+    if parts[0] == "/start":
+        send_telegram(chat_id,
+            "👋 Welcome!\n\n"
+            "I help you check products pricing and stock status in real-time \n"
+            "📌 Use:\n/product <link>\n\n"
+            "Example:\n/product https://www.example.com"
+        )
+        return "ok", 200
+
+    if parts[0] != "/product":
+        send_telegram(chat_id, "Use:\n/product <link>")
+        return "ok", 200
+
+    if len(parts) < 2:
+        send_telegram(chat_id, "Please send:\n/product https://example.com")
+        return "ok", 200
+
+    url = parts[1]
+
+    if not url.startswith("http"):
+        send_telegram(chat_id, "Invalid link. Must start with http/https")
+        return "ok", 200
+
+    print("🔗 URL:", url)
+
+    try:
+        response = requests.get(url, timeout=10)
         soup = BeautifulSoup(response.text, "html.parser")
-        text = soup.get_text()
+        page_text = soup.get_text()
 
-        if "Out Of Stock" in text or "Out of Stock" in text or "NOTIFY ME" in text:
-            print("❌ Still OUT OF STOCK")
+        title = soup.title.string if soup.title else "Product"
+
+        if "Out Of Stock" in page_text or "NOTIFY ME" in page_text:
+            status = "OUT OF STOCK"
         else:
-            print("✅ IN STOCK!!!")
-            send_telegram("HMT Kohinoor Maroon is IN STOCK! 👀")
+            status = "IN STOCK"
 
-        print("⏳ Next check in 1 hour...\n")
+        send_telegram(chat_id, f"{title}\n{status}")
 
-    else:
-        print("🌙 Bot sleeping (outside working hours)...")
+    except Exception as e:
+        print("Error:", e)
+        send_telegram(chat_id, "Error fetching product")
 
-    time.sleep(3600)
+    return "ok", 200
+
+def send_telegram(chat_id, msg):
+    requests.post(
+        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+        json={"chat_id": chat_id, "text": msg}
+    )
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
